4108c46b1754cb39a356b61779a093f92a3e4062
Chloe Lee <chloealessandra@gmail.com>
2015-09-01 10:40:12 -0400

    adds dedupe for mobile and environment variables

--- app/Console/Commands/RemoveDuplicateUsersCommand.php
+++ app/Console/Commands/RemoveDuplicateUsersCommand.php
@@ -4,7 +4,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Northstar\Models\User;
 use Config;
-
 class RemoveDuplicateUsersCommand extends Command {
     /**
      * The console command name.
@@ -34,13 +33,12 @@ class RemoveDuplicateUsersCommand extends Command {
      */
     public function fire()
     {
-
         // Get all users and sort alphabetically.
-
-        // if (isset($last_checked)) {
-            // $users_alphabetical_order = User::where('email', 'ASC') > $last_checked;
-        // } else {
+        if (null !== Config::get('last_checked')) {
+            $users_alphabetical_order = User::where('email', 'ASC') > Config::get('last_checked');
+        } else {
             $users_alphabetical_order = User::orderBy('email', 'ASC')->get();
+        }
 
         // Go through alphabetized array and compare each record to look for duplicates.
         $length = count($users_alphabetical_order);
@@ -49,11 +47,28 @@ class RemoveDuplicateUsersCommand extends Command {
             if ($users_alphabetical_order[$i]->email == $users_alphabetical_order[$i + 1]->email) {
                 $this->combine($users_alphabetical_order[$i], $users_alphabetical_order[$i + 1]);
             }
+            Config::set('last_checked', $users_alphabetical_order[$i]);
         }
-        $this->info('Deduplication complete.');
-        // $this->info(User::orderBy('email', 'ASC')->pluck('email')->toArray());
-    }
 
+        // Get all users and sort by mobile number.
+        if (null !== Config::get('last_checked')) {
+            $users_by_mobile = User::where('mobile', 'ASC') > Config::get('last_checked');
+        } else {
+            $users_by_mobile = User::orderBy('mobile', 'ASC')->get();
+        }
+
+        // Go through array sorted by mobile and compare each record to look for duplicates.
+        $length = count($users_by_mobile);
+
+        for ($i = 0; $i < $length-1; $i++) {
+            if ($users_by_mobile[$i]->mobile == $users_by_mobile[$i + 1]->mobile) {
+                $this->combine($users_by_mobile[$i], $users_by_mobile[$i + 1]);
+            }
+            Config::set('last_checked', $users_by_mobile[$i]);
+        }
+
+        $this->info('Deduplication complete.');
+    }
     /**
      * Combine fields with information from first created user and delete duplicate records.
      */
@@ -65,14 +80,16 @@ class RemoveDuplicateUsersCommand extends Command {
             $second_user = $first_user;
             $first_user = $tmp;
         }
-
         // Merge their data and save to the first user
         $updated_user = array_merge(array_filter($second_user->toArray()), array_filter($first_user->toArray()));
         $first_user->fill($updated_user)->save();
 
-
-        // $second_user->delete();
         User::destroy($second_user->_id);
-        echo "user deleted: " . $second_user->email . $second_user->_id . "\n";
+
+        if (isset($second_user->email)) {
+            echo "user deleted: " . $second_user->email . $second_user->_id . "\n";
+        } else {
+            echo "user delete: " . $second_user->mobile . $second_user->id . "\n";
+        }
     }
 }
