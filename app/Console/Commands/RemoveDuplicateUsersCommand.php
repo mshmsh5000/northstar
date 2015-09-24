1edf4f37e4c3b30e36765ac2a26220f2b1b6e98a
Chloe Lee <chloealessandra@gmail.com>
2015-09-24 12:59:04 -0400

    revert back to mongo query

--- app/Console/Commands/RemoveDuplicateUsersCommand.php
+++ app/Console/Commands/RemoveDuplicateUsersCommand.php
@@ -3,7 +3,6 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Northstar\Models\User;
-use Config;
 class RemoveDuplicateUsersCommand extends Command {
     /**
      * The console command name.
@@ -33,63 +32,66 @@ class RemoveDuplicateUsersCommand extends Command {
      */
     public function fire()
     {
-        // Get all users and sort alphabetically.
-        if (null !== Config::get('last_checked')) {
-            $users_alphabetical_order = User::where('email', 'ASC') > Config::get('last_checked');
-        } else {
-            $users_alphabetical_order = User::orderBy('email', 'ASC')->get();
+        // Find all duplicate users by email.
+        $duplicates = User::raw(function($collection) {
+            return $collection->aggregate(
+                [
+                    [
+                        '$group' => [
+                            '_id' => ['email' => '$email'
+                            ],
+                            'uniqueIds' => [
+                                '$addToSet' => '$_id'
+                            ],
+                            'count' => [
+                                '$sum' => 1
+                            ]
+                        ]
+                    ],
+                    [
+                        '$match' => [
+                            'count' => [
+                                '$gt' => 1
+                            ]
+                        ]
+                    ]
+                ],
+                [
+                    'allowDiskUse' => true
+                ]
+            );
+        });
+        // For each duplicate user, delete all records except first created record.
+        foreach ($duplicates['result'] as $user) {
+                if (count($user['uniqueIds']) > 1) {
+                    $duplicate_id = $user['uniqueIds'][0]->{'$id'};
+                    User::destroy($duplicate_id);
+                }
         }
-
-        // Go through alphabetized array and compare each record to look for duplicates.
-        $length = count($users_alphabetical_order);
-
-        for ($i = 0; $i < $length-1; $i++) {
-            if ($users_alphabetical_order[$i]->email == $users_alphabetical_order[$i + 1]->email) {
-                $this->combine($users_alphabetical_order[$i], $users_alphabetical_order[$i + 1]);
-            }
-            Config::set('last_checked', $users_alphabetical_order[$i]);
-        }
-
-        // Get all users and sort by mobile number.
-        if (null !== Config::get('last_checked')) {
-            $users_by_mobile = User::where('mobile', 'ASC') > Config::get('last_checked');
-        } else {
-            $users_by_mobile = User::orderBy('mobile', 'ASC')->get();
-        }
-
-        // Go through array sorted by mobile and compare each record to look for duplicates.
-        $length = count($users_by_mobile);
-
-        for ($i = 0; $i < $length-1; $i++) {
-            if ($users_by_mobile[$i]->mobile == $users_by_mobile[$i + 1]->mobile) {
-                $this->combine($users_by_mobile[$i], $users_by_mobile[$i + 1]);
-            }
-            Config::set('last_checked', $users_by_mobile[$i]);
-        }
-
         $this->info('Deduplication complete.');
     }
+}
     /**
      * Combine fields with information from first created user and delete duplicate records.
      */
-    public function combine($first_user, $second_user)
-    {
-        // Always make sure $first_user is the "original" user that we're going merge.
-        if ($first_user->created_at > $second_user->created_at) {
-            $tmp = $second_user;
-            $second_user = $first_user;
-            $first_user = $tmp;
-        }
-        // Merge their data and save to the first user
-        $updated_user = array_merge(array_filter($second_user->toArray()), array_filter($first_user->toArray()));
-        $first_user->fill($updated_user)->save();
+    // public function combine($first_user, $second_user)
+    // {
+    //     // Always make sure $first_user is the "original" user that we're going merge.
+    //     if ($first_user->created_at > $second_user->created_at) {
+    //         $tmp = $second_user;
+    //         $second_user = $first_user;
+    //         $first_user = $tmp;
+    //     }
+    //     // Merge their data and save to the first user
+    //     $updated_user = array_merge(array_filter($second_user->toArray()), array_filter($first_user->toArray()));
+    //     $first_user->fill($updated_user)->save();
 
-        User::destroy($second_user->_id);
+    //     User::destroy($second_user->_id);
 
-        if (isset($second_user->email)) {
-            echo "user deleted: " . $second_user->email . $second_user->_id . "\n";
-        } else {
-            echo "user deleted: " . $second_user->mobile . $second_user->id . "\n";
-        }
-    }
+    //     if (isset($second_user->email)) {
+    //         echo "user deleted: " . $second_user->email . $second_user->_id . "\n";
+    //     } else {
+    //         echo "user deleted: " . $second_user->mobile . $second_user->id . "\n";
+    //     }
+    // }
 }
