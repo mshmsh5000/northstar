382f27892a23a8f424f7212007ea4be9e9aeb036
David Furnes <david@dfurnes.com>
2016-03-04 14:52:40 -0500

    Increment a stat every time a route is matched.
    Is this overkill? I don’t know!

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace Northstar\Providers;
 
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Routing\Events\RouteMatched;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,11 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot($events);
 
-        //
+        // Update count on StatHat every time a route is hit.
+        // e.g. will increment the "northstar - v1/users/{term}/{id}" stat each
+        // time a client attempts to view a user profile through that route.
+        $events->listen(RouteMatched::class, function (RouteMatched $match) {
+            app('stathat')->ezCount(env('STATHAT_APP_NAME', 'northstar').' - '.$match->route->getUri());
+        });
     }
 }
