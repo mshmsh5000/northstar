7a7a25ca241b822597da1b0c0c2cd48b8983f9ea
David Furnes <david@dfurnes.com>
2016-03-08 13:27:39 -0500

    Update signup & reportback tests with new hotness.
    Uses Laravel 5.1’s new testing methods and the new API key helpers
    added in 7040b53.

--- tests/ReportbackTest.php
+++ tests/ReportbackTest.php
@@ -6,62 +6,6 @@ use Northstar\Services\Phoenix;
 class ReportbackTest extends TestCase
 {
     /**
-     * Additional server variables for the request.
-     *
-     * @var array
-     */
-    protected $serverVariables = [
-        'CONTENT_TYPE' => 'application/json',
-        'HTTP_Accept' => 'application/json',
-    ];
-
-    /**
-     * Headers for a typical user.
-     * @var array
-     */
-    protected $server;
-
-    /**
-     * Headers for a user who has already signed up.
-     * @var array
-     */
-    protected $signedUpServer;
-
-    /**
-     * Headers for a user who has already reported back.
-     * @var array
-     */
-    protected $reportedBackServer;
-
-    /**
-     * Migrate database and set HTTP headers.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        // Migrate & seed database
-        $this->artisan('migrate');
-        $this->seed();
-
-        // Prepare server headers
-        $this->server = [
-            'HTTP_X-DS-REST-API-Key' => 'abc4324',
-            'HTTP_Authorization' => 'Bearer '.User::find('5430e850dt8hbc541c37tt3d')->login()->key,
-        ];
-
-        $this->signedUpServer = [
-            'HTTP_X-DS-REST-API-Key' => 'abc4324',
-            'HTTP_Authorization' => 'Bearer '.User::find('5480c950bffebc651c8b456f')->login()->key,
-        ];
-
-        $this->reportedBackServer = [
-            'HTTP_X-DS-REST-API-Key' => 'abc4324',
-            'HTTP_Authorization' => 'Bearer '.User::find('bf1039b0271bcc636aa5477a')->login()->key,
-        ];
-    }
-
-    /**
      * Test for submitting a new campaign report back.
      * POST /user/campaigns/:nid/reportback
      *
@@ -69,19 +13,11 @@ class ReportbackTest extends TestCase
      */
     public function testSubmitCampaignReportback()
     {
-        $payload = [
-            'campaign_id' => 123,
-            'quantity' => 10,
-            'why_participated' => 'I love helping others',
-            'file' => 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAMCA',
-            'caption' => 'Here I am helping others.',
-        ];
+        $user = User::create(['drupal_id' => '512312']);
 
         // For testing, we'll mock successful Phoenix API responses.
         $phoenix = $this->mock(Phoenix::class);
-        $phoenix->shouldReceive('createReportback')->once()->andReturn([
-            '127',
-        ]);
+        $phoenix->shouldReceive('createReportback')->once()->andReturn(['127']);
         $phoenix->shouldReceive('getReportback')->once()->andReturn([
             'data' => [
                 'id' => 127,
@@ -89,13 +25,16 @@ class ReportbackTest extends TestCase
             ],
         ]);
 
-        $response = $this->call('POST', 'v1/reportbacks', [], [], [], $this->signedUpServer, json_encode($payload));
+        $this->asUser($user)->withAuthorizedScopes(['user'])->json('POST', 'v1/reportbacks', [
+            'campaign_id' => 123,
+            'quantity' => 10,
+            'why_participated' => 'I love helping others',
+            'file' => 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAMCA',
+            'caption' => 'Here I am helping others.',
+        ]);
 
         // The response should return a 200 OK status code
-        $this->assertEquals(200, $response->getStatusCode());
-
-        // Response should be valid JSON
-        $content = $response->getContent();
-        $this->assertJson($content);
+        $this->assertResponseStatus(200);
+        $this->seeJson();
     }
 }
