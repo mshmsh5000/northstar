731ea3c628995ab3daf483441682863c4470f610
David Furnes <david@dfurnes.com>
2016-04-11 15:22:02 -0400

    Add some tests for OAuth endpoints.

---
+++ tests/OAuthTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use Northstar\Models\Client;
+use Northstar\Models\User;
+
+class OAuthTest extends TestCase
+{
+    /**
+     * Test that the password grant rejects invalid credentials.
+     */
+    public function testPasswordGrantWithInvalidCredentials()
+    {
+        $user = User::create(['email' => 'login-test@dosomething.org', 'password' => 'secret']);
+        $client = Client::create(['app_id' => 'phpunit']);
+
+        $this->post('v2/auth/token', [
+            'grant_type' => 'password',
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+            'username' => $user->email,
+            'password' => 'letmein',
+        ]);
+
+        $this->assertResponseStatus(401);
+    }
+
+    /**
+     * Test that the password grant provides a JWT for valid credentials.
+     */
+    public function testPasswordGrant()
+    {
+        $user = User::create(['email' => 'login-test@dosomething.org', 'password' => 'secret']);
+        $client = Client::create(['app_id' => 'phpunit']);
+
+        $this->post('v2/auth/token', [
+            'grant_type' => 'password',
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+            'username' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonStructure([
+            'token_type',
+            'expires_in',
+            'access_token',
+            'refresh_token',
+        ]);
+    }
+
+    /**
+     * Test that the client credentials grant provides a JWT for valid credentials.
+     */
+    public function testClientCredentials()
+    {
+        $client = Client::create(['app_id' => 'phpunit']);
+
+        $this->post('v2/auth/token', [
+            'grant_type' => 'client_credentials',
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+        ]);
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonStructure([
+            'token_type',
+            'expires_in',
+            'access_token',
+        ]);
+    }
+}
