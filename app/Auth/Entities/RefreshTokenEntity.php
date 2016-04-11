51531a116186ad5c4a06ec439232c01a0363aa94
David Furnes <david@dfurnes.com>
2016-04-11 14:27:09 -0400

    Refresh tokens should not expire.

--- app/Auth/Entities/RefreshTokenEntity.php
+++ app/Auth/Entities/RefreshTokenEntity.php
@@ -9,4 +9,14 @@ use League\OAuth2\Server\Entities\Traits\RefreshTokenTrait;
 class RefreshTokenEntity implements RefreshTokenEntityInterface
 {
     use RefreshTokenTrait, EntityTrait;
+
+    /**
+     * We do not want our refresh tokens to expire.
+     *
+     * @return bool
+     */
+    public function isExpired()
+    {
+        return false;
+    }
 }
