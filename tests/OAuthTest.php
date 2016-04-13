bf8f79c23df648ce8dd81b98927672611cea0ad3
David Furnes <david@dfurnes.com>
2016-04-13 10:25:32 -0400

    Add test for refresh token grant.

--- tests/OAuthTest.php
+++ tests/OAuthTest.php
@@ -117,4 +117,51 @@ class OAuthTest extends TestCase
             'access_token',
         ]);
     }
+
+    /**
+     * Test that the refresh token grant provides a new JWT in exchange for
+     * an unused refresh token, and then invalidates that refresh token.
+     */
+    public function testRefreshTokenGrant()
+    {
+        $user = User::create(['email' => 'login-test@dosomething.org', 'password' => 'secret']);
+        $client = Client::create(['app_id' => 'phpunit']);
+
+        $this->post('v2/auth/token', [
+            'grant_type' => 'password',
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+            'username' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        // Get the provided refresh token.
+        $refreshToken = $this->decodeResponseJson()['refresh_token'];
+
+        // Get a new access token using the refresh token.
+        $this->post('v2/auth/token', [
+            'grant_type' => 'refresh_token',
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+            'refresh_token' => $refreshToken,
+        ]);
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonStructure([
+            'token_type',
+            'expires_in',
+            'access_token',
+            'refresh_token',
+        ]);
+
+        // And now, verify that that refresh token has been consumed.
+        $this->post('v2/auth/token', [
+            'grant_type' => 'refresh_token',
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+            'refresh_token' => $refreshToken,
+        ]);
+
+        $this->assertResponseStatus(400);
+    }
 }
