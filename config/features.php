2a33bb34a1085604c427e2a42b6732d8536370ad
David Furnes <david@dfurnes.com>
2016-06-01 15:01:47 -0400

    Add feature flag config.

---
+++ config/features.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Feature Flags
+    |--------------------------------------------------------------------------
+    |
+    | This file is a custom addition to Northstar for storing feature flags, so
+    | features can be conditionally toggled on and off per environment.
+    |
+    */
+
+
+];
