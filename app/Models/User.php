83f799607b432f18c98e2085fae50a22bc2793a4
David Furnes <david@dfurnes.com>
2016-07-05 14:04:25 -0400

    We need separate arrays for unique vs. plain indexed fields. Previously, we were triggering "index upsert" errors when adding a user with an existing source. That's definitely not right, so it seems like we need to be able to tell the difference between indexed fields (to allow performant searching), and uniquely indexed fields (to prevent upserting an account that shouldn't be).

--- app/Models/User.php
+++ app/Models/User.php
@@ -101,6 +101,18 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var array
      */
+    public static $uniqueIndexes = [
+        '_id', 'drupal_id', 'email', 'mobile',
+    ];
+
+    /**
+     * Attributes that can be queried when filtering.
+     *
+     * This array is manually maintained. It does not necessarily mean that
+     * any of these are actual indexes on the database... but they should be!
+     *
+     * @var array
+     */
     public static $indexes = [
         '_id', 'drupal_id', 'email', 'mobile', 'source',
     ];
