d004d46c40f22bc0bf14a08a8def7dc6a5963589
David Furnes <david@dfurnes.com>
2016-07-07 14:23:08 -0400

    Add v2 for client resource.

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Northstar\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Northstar\Models\Client;
+use Northstar\Http\Transformers\ClientTransformer;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ClientController extends Controller
+{
+    /**
+     * @var ClientTransformer
+     */
+    protected $transformer;
+
+    public function __construct(ClientTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+
+        $this->middleware('role:admin');
+    }
+
+    /**
+     * Display a listing of the resource.
+     * GET /v2/clients
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $keys = Client::all();
+
+        return $this->collection($keys);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * POST /v2/clients
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     * @throws HttpException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'client_id' => 'required|unique:clients,client_id',
+            'scope' => 'array|scope', // @see Scope::validateScopes
+        ]);
+
+        $key = Client::create($request->only('client_id', 'scope'));
+
+        return $this->item($key, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /v2/clients/:client_id
+     *
+     * @param Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        return $this->item($client);
+    }
+
+    /**
+     * Update the specified resource.
+     * PUT /v2/clients/:client_id
+     *
+     * @param Client $client
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Client $client, Request $request)
+    {
+        $this->validate($request, [
+            'scope' => 'array|scope', // @see Scope::validateScopes
+        ]);
+
+        $client->update($request->all());
+
+        return $this->item($client);
+    }
+
+    /**
+     * Delete an API key resource.
+     * DELETE /v2/clients/:client_id
+     *
+     * @param Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+
+        return $this->respond('Deleted key.', 200);
+    }
+}
---
+++ app/Http/Transformers/ClientTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Northstar\Http\Transformers;
+
+use Northstar\Models\Client;
+use League\Fractal\TransformerAbstract;
+
+class ClientTransformer extends TransformerAbstract
+{
+    /**
+     * @param Client $client
+     * @return array
+     */
+    public function transform(Client $client)
+    {
+        return [
+            'client_id' => $client->client_id,
+            'client_secret' => $client->client_secret,
+            'scope' => $client->scope,
+
+            'updated_at' => $client->updated_at->toISO8601String(),
+            'created_at' => $client->created_at->toISO8601String(),
+        ];
+    }
+}
