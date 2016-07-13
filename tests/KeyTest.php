de155e6adff7b1b256d4be2bd7c258eba05a1f75
David Furnes <david@dfurnes.com>
2016-07-13 13:27:29 -0400

    Add functional tests for public key endpoint.

---
+++ tests/KeyTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Northstar\Models\User;
+
+class KeyTest extends TestCase
+{
+    /**
+     * Test retrieving multiple users.
+     * GET /users
+     *
+     * @return void
+     */
+    public function testKeyNotVisibleToUserRole()
+    {
+        $this->get('v2/key');
+        $this->assertResponseStatus(401);
+
+        $this->asNormalUser()->get('v2/key');
+        $this->assertResponseStatus(401);
+    }
+
+    /**
+     * Test retrieving multiple users.
+     * GET /users
+     *
+     * @return void
+     */
+    public function testKeyNotVisibleToStaffRole()
+    {
+        // Make a staff user & some test users.
+        $staff = factory(User::class, 'staff')->create();
+
+        $this->asUser($staff, ['role:staff'])->get('v2/key');
+        $this->assertResponseStatus(401);
+    }
+
+    /**
+     * Test retrieving multiple users.
+     * GET /users
+     *
+     * @return void
+     */
+    public function testKeyVisibleToAdminRole()
+    {
+        $this->asAdminUser()->get('v2/key');
+        $this->assertResponseStatus(200);
+        $this->seeJsonStructure([
+            'algorithm',
+            'issuer',
+            'public_key',
+        ]);
+    }
+}
