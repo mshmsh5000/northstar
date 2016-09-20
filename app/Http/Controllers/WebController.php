2f1a413f3ec128dba01975e8c13e2969f17805dc
David Furnes <david@dfurnes.com>
2016-09-20 17:58:00 -0400

    Implement registration form.

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Northstar\Auth\Registrar;
+use Northstar\Exceptions\NorthstarValidationException;
+use Northstar\Models\User;
 
 class WebController extends BaseController
 {
@@ -19,16 +22,26 @@ class WebController extends BaseController
     protected $auth;
 
     /**
+     * The registrar.
+     *
+     * @var Registrar
+     */
+    protected $registrar;
+
+    /**
      * Make a new WebController, inject dependencies,
      * and set middleware for this controller's methods.
      *
      * @param \Illuminate\Contracts\Auth\Factory $auth
+     * @param Registrar $registrar
      */
-    public function __construct(Auth $auth)
+    public function __construct(Auth $auth, Registrar $registrar)
     {
         $this->auth = $auth;
+        $this->registrar = $registrar;
 
         $this->middleware('auth:web', ['only' => ['home', 'getLogout']]);
+        $this->middleware('guest:web', ['except' => ['home', 'getLogout']]);
     }
 
     /**
@@ -102,15 +115,27 @@ class WebController extends BaseController
      *
      * @param Request $request
      * @return \Illuminate\Http\Response
+     * @throws NorthstarValidationException
      */
     public function postRegister(Request $request)
     {
-        $this->validate($request, [
+        // If a user exists but has not set a password yet, allow them to
+        // "register" to set a new password on their account.
+        $existing = $this->registrar->resolve($request);
+        if ($existing && $existing->hasPassword()) {
+            throw new NorthstarValidationException(['email' => 'A user with that email or mobile has already been registered.']);
+        }
+
+        $this->registrar->validate($request, $existing, [
             'first_name' => 'required',
-            'email' => 'required|email',
             'birthdate' => 'required|date',
-            'mobile' => 'mobile',
             'password' => 'required|confirmed|min:6',
         ]);
+
+        // Register and login the user.
+        $user = $this->registrar->register($request->except(User::$internal), $existing);
+        $this->auth->guard('web')->login($user, true);
+
+        return redirect()->intended('/');
     }
 }
