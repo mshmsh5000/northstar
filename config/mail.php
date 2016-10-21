4251885c70d05f7ac6f5a37a1da861d1ab3fe8f7
David Furnes <david@dfurnes.com>
2016-10-21 14:05:51 -0400

    Configure env variables for testing email w/ Mailgun.

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,10 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => [
+        'address' => env('MAIL_ADDRESS'),
+        'name' => env('MAIL_NAME', 'DoSomething.org'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
