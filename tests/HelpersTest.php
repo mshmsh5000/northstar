5f2af8b6de976da1417b28c5f0b4483e053071f4
David Furnes <david@dfurnes.com>
2016-11-09 17:46:58 -0500

    Add a client_id helper.

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -1,11 +1,13 @@
 <?php
 
+use Northstar\Models\Client;
+
 class HelpersTest extends TestCase
 {
     /** @test */
     public function testRouteHasAttachedMiddleware()
     {
-        $request = $this->get('/login');
+        $this->get('/login');
 
         // It should be able to check if in a middleware group.
         $this->assertTrue(has_middleware('web'));
@@ -13,5 +15,29 @@ class HelpersTest extends TestCase
 
         // ...or just if it has any middleware at all!
         $this->assertTrue(has_middleware());
+    }
+
+    /** @test */
+    public function testGetClientId()
+    {
+        // Web requests should report as 'northstar'.
+        $this->get('/register');
+        $this->assertEquals(client_id(), 'northstar');
+
+        // Make a request using legacy API header.
+        $client = Client::create(['client_id' => 'legacy_client', 'scope' => 'user']);
+        $this->withLegacyApiKey($client)->json('POST', '/v1/auth/register', [
+            'email' => $this->faker->safeEmail,
+            'password' => $this->faker->password,
+        ]);
+        $this->assertEquals(client_id(), 'legacy_client');
+
+
+        // And make a request using OAuth.
+        $this->asNormalUser()->json('POST', '/v1/auth/register', [
+            'email' => $this->faker->safeEmail,
+            'password' => $this->faker->password,
+        ]);
+        $this->assertEquals(client_id(), 'phpunit');
     }
 }
