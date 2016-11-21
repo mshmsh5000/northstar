1a7d2a1c0126d919b529899a44cee1fe5dfc1606
David Furnes <david@dfurnes.com>
2016-11-21 13:12:17 -0500

    Use new date helper in Phoenix API class.

--- app/Services/Phoenix.php
+++ app/Services/Phoenix.php
@@ -117,7 +117,7 @@ class Phoenix
         $payload = $user->toArray();
 
         // Format user object for consumption by Drupal API.
-        $payload['birthdate'] = date('Y-m-d', strtotime($user->birthdate));
+        $payload['birthdate'] = format_date($user->birthdate, 'Y-m-d');
         $payload['user_registration_source'] = $user->source;
         $payload['password'] = $password;
 
