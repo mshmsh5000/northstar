8e5ca42f9671d1c5908c03e9e610c3e617df7402
David Furnes <david@dfurnes.com>
2016-11-22 14:54:55 -0500

    Add favicon and touch icon.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('apple-touch-icon-precomposed.png') }}">
+
     <link rel="stylesheet" href="{{ asset('dist/app.css') }}">
     <script src="{{ asset('dist/modernizr.js') }}"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
