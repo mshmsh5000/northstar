3ccd0eeaaa8c900c7e34c6eed75b41c1e6a147b7
Joe Kent <thedeadlybutter@gmail.com>
2017-02-08 11:23:57 -0500

    Adds es-mx translations (#545)

---
+++ resources/lang/es-mx/auth.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    'greeting' => [
+        'lets_do_this' => '¡Hagámoslo!',
+    ],
+    'log_in' => [
+        'default' => 'Iniciar Sesión',
+        'existing' => 'Iniciar sesión en una cuenta existente',
+        'create' => 'Crea una cuenta DoSomething.org',
+        'submit' => 'Crear Nueva Cuenta',
+    ],
+    'get_started' => [
+        'log_in' => '¡Inicia sesión para comenzar!',
+        'create_account' => 'Para comenzar, ¡crea una cuenta DoSomething.org!',
+    ],
+    'validation' => [
+        'issues' => 'Hmm, there were some issues with that submission:',
+        'optional' => '(opcional)',
+        'auth' => [
+            'email' => 'Proporciona una dirección válida de correo electrónico.',
+        ],
+        'signup' => [
+            'birthday' => 'Necesitamos tu cumpleaños.',
+            'first_name' => 'Necesitamos tu nombre.',
+            'email' => 'Necesitamos un correo electrónico válido.',
+            'password_length' => 'Debe tener al menos 6 caracteres.',
+        ],
+        'placeholder' => [
+            'call_you' => '¿Qué te llamamos?',
+            'birthday' => 'DD/MM/AAAA',
+            'password' => '6 o más caracteres... ¡haz que sea difícil!',
+            'double_checking' => '¡Solo nos estábamos asegurando!',
+        ],
+    ],
+    'fields' => [
+        'email' => 'Dirección de correo electrónico',
+        'mobile' => 'Número de celular',
+        'email_or_mobile' => 'Dirección de correo electrónico o número de celular',
+        'first_name' => 'Nombre',
+        'birthday' => 'Cumpleaños',
+        'password' => 'Contraseña',
+        'confirm_password' => 'Confirmar la contraseña',
+        'new_password' => 'Nueva contraseña',
+        'confirm_new_password' => 'Confirmar la contraseña',
+    ],
+    'forgot_password' => [
+        'header' => '¿Olvidaste la contraseña?',
+        'instructions' => 'We\'ve all been there. Reset by entering your email.',
+        'request' => 'Request New Password',
+    ],
+    'footnote' => [
+        'create' => 'Crear una cuenta significa que aceptas nuestros',
+        'terms_of_service' => 'Términos del servicio',
+        'privacy_policy' => 'Política de Privacidad',
+        'messaging' => 'y aceptas recibir nuestras noticias semanales. Pueden aplicarse cargos por mensajes y datos. Envía la palabra STOP en un mensaje de texto para dejar de recibir mensajes y la palabra HELP para solicitar ayuda.',
+    ],
+];
