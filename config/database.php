cd07cd942081ecf0e6bea5cba721e1302924f769
blisteringherb <morgan@solosuenos.com>
2017-02-09 14:28:59 -0500

    Reverting repl set formatting change'

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'database' => env('DB_NAME', 'userapi'),
             'options'  => [
-                'replicaSet' => env('DB_REPL_SET_NAME', 'rs0'),
+                env('DB_REPL_SET_NAME') ? ['replicaSet' => env('DB_REPL_SET_NAME', 'rs0')] : [],
                 'database' => env('DB_NAME', 'userapi'),
             ],
         ],
