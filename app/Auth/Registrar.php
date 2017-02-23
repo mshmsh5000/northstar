4fa3897f390f6db54a6edc7519a117f89b90488b
David Furnes <david@dfurnes.com>
2017-02-23 12:20:03 -0500

    Provide expected arguments to the authentication events.

--- app/Auth/Registrar.php
+++ app/Auth/Registrar.php
@@ -152,13 +152,13 @@ class Registrar
     public function validateCredentials($user, array $credentials)
     {
         if (! $user) {
-            event(\Illuminate\Auth\Events\Failed::class);
+            event(new \Illuminate\Auth\Events\Failed($user, $credentials));
 
             return false;
         }
 
         if ($this->hasher->check($credentials['password'], $user->password)) {
-            event(\Illuminate\Auth\Events\Login::class);
+            event(new \Illuminate\Auth\Events\Login($user, false));
 
             return true;
         }
@@ -169,13 +169,13 @@ class Registrar
             $user->password = $credentials['password'];
             $user->save();
 
-            event(\Illuminate\Auth\Events\Login::class);
+            event(new \Illuminate\Auth\Events\Login($user, false));
 
             return true;
         }
 
         // Well, looks like we couldn't authenticate...
-        event(\Illuminate\Auth\Events\Failed::class);
+        event(new \Illuminate\Auth\Events\Failed($user, $credentials));
 
         return false;
     }
