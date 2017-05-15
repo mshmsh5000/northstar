812cc5688043e710ae5538427e62cd63ea0c914c
David Furnes <david@dfurnes.com>
2017-05-15 09:57:19 -0400

    Remove voting app ID fields from fillable array.
    We wipe these instances each year, so there isn't a ton of value in
    continuing to record these.

--- app/Models/User.php
+++ app/Models/User.php
@@ -86,7 +86,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         'addr_street1', 'addr_street2', 'addr_city', 'addr_state', 'addr_zip',
         'country', 'language',
 
-        'mobilecommons_id', 'mobilecommons_status', 'cgg_id', 'drupal_id', 'agg_id',
+        'mobilecommons_id', 'mobilecommons_status', 'drupal_id',
         'facebook_id', 'slack_id',
 
         'parse_installation_ids',
