efbc75cf8488fa177e2959e47de5a10c74023348
David Furnes <david@dfurnes.com>
2017-06-07 14:49:12 -0400

    Add test case for dropping nulls.

---
+++ tests/Models/ModelTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Northstar\Models\User;
+
+class ModelTest extends TestCase
+{
+    /** @test */
+    public function it_should_unset_null_fields()
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create([
+            'mobile' => $this->faker->phoneNumber,
+            'last_name' => null,
+        ]);
+
+        // Now, unset some fields.
+        $user->mobile = null;
+        $user->last_name = null;
+        $user->save();
+
+        // Make sure the field is unset on the actual document.
+        $document = $this->getMongoDocument('users', $user->id);
+        $this->assertArrayNotHasKey('mobile', $document);
+        $this->assertArrayNotHasKey('last_name', $document);
+    }
+}
