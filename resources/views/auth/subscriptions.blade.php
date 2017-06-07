c181b6183d60496c156122ffe0c8b5bd8d26fee8
Shae Smith <sbsmith86@gmail.com>
2017-06-07 15:27:17 -0400

    Add gladiator settings to the .env.example

--- resources/views/auth/subscriptions.blade.php
+++ resources/views/auth/subscriptions.blade.php
@@ -4,16 +4,16 @@
 
 @section('content')
 
-	<div class="container__block -centered ">
-		<p>Don’t want to receive emails about this competition?</p>
+    <div class="container__block -centered ">
+        <p>Don’t want to receive emails about this competition?</p>
 
-		<form role="form" method="POST" class="form-actions -padded" action="{{ url('unsubscribe') }}">
+        <form role="form" method="POST" class="form-actions -padded" action="{{ url('unsubscribe') }}">
             <input name="_token" type="hidden" value="{{ csrf_token() }}">
 
             <input type="hidden" name="competition" value="{{ Request::get('competition_id') }}">
             <input type="hidden" name="user" value="{{ Request::get('northstar_id') }}">
-			<input type="submit" class="button" value="{{ trans('Unsubscribe') }}" />
-		</form>
-	</div>
+            <input type="submit" class="button" value="{{ trans('Unsubscribe') }}" />
+        </form>
+    </div>
 
 @stop
