bdcf51ab94e05255262d030af7d6a32fc1367eef
David Furnes <david@dfurnes.com>
2017-07-11 14:46:55 -0400

    Add named prefix to prevent conflict.
    Because we have multiple 'user' resources, the named routes were conflicting (surprised they weren't all along, but bug surfaced with 5.3 upgrade.)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,7 +39,7 @@ $router->group(['namespace' => 'Web', 'guard' => 'web', 'middleware' => ['web']]
 });
 
 // API experience for https://nortstar.dosomething.org/v2/
-$router->group(['prefix' => 'v2', 'middleware' => ['api']], function () use ($router) {
+$router->group(['prefix' => 'v2', 'as' => 'v2.', 'middleware' => ['api']], function () use ($router) {
     // Authentication
     $router->post('auth/token', 'OAuthController@createToken');
     $router->get('auth/info', 'OAuthController@info');
@@ -65,7 +65,7 @@ $router->group(['prefix' => 'v2', 'middleware' => ['api']], function () use ($ro
 });
 
 // API experience for https://northstar.dosomething.org/v1/
-$router->group(['prefix' => 'v1', 'middleware' => ['api']], function () use ($router) {
+$router->group(['prefix' => 'v1', 'as' => 'v1.', 'middleware' => ['api']], function () use ($router) {
     // Authentication
     $router->post('auth/token', 'Legacy\AuthController@createToken');
     $router->post('auth/invalidate', 'Legacy\AuthController@invalidateToken');
