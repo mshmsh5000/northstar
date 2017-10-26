772aea8c43adfc4e6e19154a773dbc7ab601e18c
Joe Kent <thedeadlybutter@gmail.com>
2017-10-26 14:00:26 -0400

    adding customer io service (#664)
    * adding customer io service
    
    * its php not javascript
    
    * Style fixes
    
    * adding default url for customer io
    
    * A few more fixes
    
    * style fixes

---
+++ app/Services/CustomerIo.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Northstar\Services;
+
+use GuzzleHttp\Client;
+use Northstar\Models\User;
+
+class CustomerIo
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $url = config('services.customerio.url');
+
+        $this->client = new Client([
+            'base_uri' => $url,
+            'defaults' => [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * Get the auth parameters required to make the request.
+     * @return array
+     */
+    private function getAuthParams()
+    {
+        $username = config('services.customerio.username');
+        $password = config('services.customerio.password');
+
+        return [
+            $username,
+            $password,
+        ];
+    }
+
+    /**
+     * Update a user in Customer.io
+     *
+     * @param  User   $user
+     * @return bool   $success - Whether the update was a success.
+     */
+    public function updateProfile(User $user)
+    {
+        $response = $this->client->put('customers/'.$user->id, [
+            'json' => $user->toBlinkPayload(),
+            'auth' => $this->getAuthParams(),
+        ]);
+
+        return $response->getStatusCode() === 200;
+    }
+}
