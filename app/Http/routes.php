d5825788d398a5dff8f923e9207b57a3249a741c
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-01 13:17:07 -0400

    change naming of password.reset for actual reset form route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,9 +36,9 @@ $router->group(['namespace' => 'Web', 'guard' => 'web', 'middleware' => ['web']]
     $router->post('register', 'AuthController@postRegister');
 
     // Password Reset
-    $this->get('password/reset', ['as' => 'password.reset', 'uses' => 'ForgotPasswordController@showLinkRequestForm']);
+    $this->get('password/reset', 'ForgotPasswordController@showLinkRequestForm');
     $this->post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
-    $this->get('password/reset/{token}', 'ResetPasswordController@showResetForm');
+    $this->get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'ResetPasswordController@showResetForm']);
     $this->post('password/reset', 'ResetPasswordController@reset');
 });
 
