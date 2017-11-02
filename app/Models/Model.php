d866488164a1ed6fc6989bae54df298375e00c5e
David Furnes <david@dfurnes.com>
2017-11-02 10:25:39 -0400

    Remove date hacks on model.
    This felt like a bit too deep a hack, so instead only doing the fancy parsing on the birthdate field.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -106,49 +106,4 @@ class Model extends BaseModel
 
         return true;
     }
-
-    /**
-     * Convert a DateTime to a string that can be
-     * stored in the database.
-     *
-     * @param  \DateTime|int  $value
-     * @return string
-     */
-    public function fromDateTime($value)
-    {
-        $value = $this->asDateTime($value);
-
-        return new UTCDateTime($value->getTimestamp() * 1000);
-    }
-
-    /**
-     * Return a timestamp as DateTime object.
-     *
-     * @param  mixed  $value
-     * @return \DateTime|Carbon
-     */
-    protected function asDateTime($value)
-    {
-        try {
-            return parent::asDateTime($value);
-        } catch (InvalidArgumentException $e) {
-            return $this->asDateTimeFallback($value);
-        }
-    }
-
-    /**
-     * Fallback to try to parse poorly formatted date strings, or
-     * return `null` if it's hopeless.
-     *
-     * @param $value
-     * @return Carbon|null
-     */
-    protected function asDateTimeFallback($value)
-    {
-        try {
-            return Carbon::parse($value);
-        } catch (Exception $e) {
-            return null;
-        }
-    }
 }
