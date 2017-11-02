0e28d560b07b8a75b37da0a7c2d710225ad8b291
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-11-02 10:36:39 -0400

    refactoring reset link generation, adding user email as param

--- app/Auth/Notifications/ResetPassword.php
+++ app/Auth/Notifications/ResetPassword.php
@@ -11,14 +11,14 @@ class ResetPassword extends ResetPasswordNotification
      * Build the mail representation of the notification.
      * (This is our custom override of the default email message.)
      *
-     * @param  mixed  $notifiable
+     * @param  User $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your DoSomething.org account. Here is the link to reset your password:')
-            ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
+            ->action('Reset Password', route('password.reset', [$this->token, 'email' => $notifiable->email]))
             ->line('This link will expire in 24 hours. Once you click the button above, you will be asked to reset your password on the page. If you did not request a password reset, you can ignore this email. Your password will not change and your account is safe.')
             ->line('If you have further questions, please reach out to help@dosomething.org.');
     }
